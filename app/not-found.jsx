58a8c5f5492bcda216cbbd8da58415fb35f1dec3
ansariali <=>
2025-05-23 23:54:05 +0530

    Done with Not found Page

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+import { Title } from "@/components/server-components/Title";
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-[50vh] grid items-center">
+      <Title title={"Not Found | 404 page not found"} />
+    </div>
+  );
+}
