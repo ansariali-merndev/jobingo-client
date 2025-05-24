d2bb5f41f5fbce596aa2242d6cb00bd73e0bf4a0
ansariali <=>
2025-05-24 13:19:55 +0530

    axios updated

--- data/axios.js
+++ data/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://jobingo-server.onrender.com",
   withCredentials: true,
 });
 
